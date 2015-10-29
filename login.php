2b0b27c731894705d3029f6fa6a53150f98e8a89
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2015-10-29 20:55:50 +0530

    ads

--- login.php
+++ login.php
@@ -5,28 +5,54 @@
 		$password = $_POST['password'];
 		$server   = "{webmail.nitt.edu:143/novalidate-cert}";
 			$imap = @imap_open($server,$username, $password,NULL,0); 
-		    var_dump(imap_errors());
+		    imap_errors();
 		if($imap)
 		{
-			echo 'Web-mail Authentication SUCCESS<br><br>';
+			echo '<script>alert("success");</script>';
 		}
 		else
 		{
-			echo 'INCORRECT roll number or password<br><br>';
+			echo '<script>alert("fail");</script>';
 		}
 	 }
 	
 	?>
 	<html>
 	<head>
-        <title>Sriranganathan</title>	
+   
+    <title>CR elections</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<link rel = "stylesheet" href = "election.css" />
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type="text/css">
+		
 	 </head>
 	
 	<body>
-		<form id="form" name="form" method="post" action="">
-		<input type="text" name="username" placeholder="rollnumber">
-		<input type="password" name="password" placeholder="password">
-		<input type="submit" name="submit" value="submit">
-		</form>
+		<div class = "page-header" id = "header">
+			CR Elections
+		</div>
+		<div class = "row" id = "registration_form">
+				<br/><br/>
+				<form id="form" name="form" method="post" action="">
+				<label>Rollno</label>
+				<div class = "form-group">
+					<input class = "form-control" type="text" name="username" placeholder="rollnumber">
+				</div>
+				<label>Password</label>
+				<div class = "form-group">
+					<input type="password" class="form-control" name="password" placeholder="password">
+				</div>
+				<br/>
+				<button type = "submit" class = "btn btn-info" name="submit" >submit</button>
+				
+				</form>
+		</div>
 	</body>
 	</html>
